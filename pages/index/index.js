3ef99fae6b74bc849ba59b587d8627129cf937f0
hilary <hanhan0602@gmail.com>
2018-12-17 11:12:31 +0800

    預報列表UI

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,39 @@
 Page({
   data: {
-    temperature: 7,
-    weather: "Chilly",
+    temperature: -8,
+    location: "Bejing",
+    weather: "Snow",
+    predictArray: [{
+      weekDay: "Sunday",
+      temperature: 4,
+      weather: "Sunny",
+    }, {
+      weekDay: "Monday",
+      temperature: 7,
+      weather: "Chilly",
+    }, {
+      weekDay: "Tuesday",
+      temperature: 7,
+      weather: "Chilly",
+    }, {
+      weekDay: "Wednesday",
+      temperature: 3,
+      weather: "Rain",
+    }, {
+      weekDay: "Thursday",
+      temperature: 6,
+      weather: "Sunny",
+    }
+    // , {
+    //   weekDay: "Friday",
+    //   temperature: 2,
+    //   weather: "Sunny",
+    // }, {
+    //   weekDay: "Saturday",
+    //   temperature: 5,
+    //   weather: "Cloudy",
+    // }
+    ],
   },
   onLoad: function () {
     this.setData({
