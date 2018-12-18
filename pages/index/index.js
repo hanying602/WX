5690f07608c077bacd7d64c8c4f40fbadbc37e02
hilary <hanhan0602@gmail.com>
2018-12-18 18:27:21 +0800

    1.定位地點改接騰訊api 2.預測列表改成scrollview 3.dark api加上language param zh-tw

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,5 @@
 Page({
   data: {
-    temperature: 8,
-    location: "Bejing",
-    weather: "Snow",
-    predictArray: [{
-      weekDay: "Sunday",
-      temperature: 4,
-      weather: "Sunny",
-    }, {
-      weekDay: "Monday",
-      temperature: 7,
-      weather: "Chilly",
-    }, {
-      weekDay: "Tuesday",
-      temperature: 7,
-      weather: "Chilly",
-    }, {
-      weekDay: "Wednesday",
-      temperature: 3,
-      weather: "Rain",
-    }, {
-      weekDay: "Thursday",
-      temperature: 6,
-      weather: "Sunny",
-    }
-    // , {
-    //   weekDay: "Friday",
-    //   temperature: 2,
-    //   weather: "Sunny",
-    // }, {
-    //   weekDay: "Saturday",
-    //   temperature: 5,
-    //   weather: "Cloudy",
-    // }
-    ],
   },
   
   onLoad: function () {
@@ -43,21 +9,23 @@ Page({
       success: function (res) {
         var latitude = res.latitude
         var longitude = res.longitude
+        var locationStr = latitude + ',' + longitude
         wx.request({
-          url: 'https://api.darksky.net/forecast/b71ca68cca4c59a4e1197efd72c98656/'+latitude+','+longitude,
+          url: 'https://api.darksky.net/forecast/b71ca68cca4c59a4e1197efd72c98656/' + locationStr +'?&lang=zh-tw',
           data: {},
           method: 'GET',
           success: function (res) {
             var predictDatas = res.data.daily.data;
-            for (var i = 0; i < 5; i++) {
+            for (var i = 0; i < predictDatas.length; i++) {
               predictDatas[i].time = timeConverter(predictDatas[i].time)
-              predictDatas[i].temperatureHigh = Math.round((predictDatas[i].temperatureHigh-32)*5/9)
+              predictDatas[i].temperatureHigh = Math.round((predictDatas[i].temperatureHigh - 32) * 5 / 9)
+              predictDatas[i].temperatureLow = Math.round((predictDatas[i].temperatureLow - 32) * 5 / 9)
+              predictDatas[i].summary = (predictDatas[i].summary).split("。")[0]
             }
             that.setData({
               temperature: Math.round((res.data.currently.temperature - 32) * 5 / 9),
-              location: (res.data.timezone).split("/")[1],
               weather: res.data.currently.summary,
-              predictArray: predictDatas.slice(0,5),
+              predictArray: predictDatas,
             })
           },
           fail: function (res) {
@@ -68,16 +36,41 @@ Page({
             console.log(res.data)
           }
         })
+      wx.request({
+          url: 'https://apis.map.qq.com/ws/geocoder/v1/?l&get_poi=1',
+          data: {
+            "key": "WO5BZ-ANDWX-WV54Q-Z27A2-SQL2Z-RDBNX",
+            "location": locationStr
+          },
+          method: 'GET',
+           
+          success: function (res) {
+            that.setData({
+              location: res.data.result.address_component.city + res.data.result.address_component.district,
+            })
+          },
+          fail: function () {
+            console.log('submit fail');
+          },
+          complete: function (res) {
+            console.log('submit complete');
+            console.log(res.data)
+          }
+        })
+              
       }
     })
   }
+
+  
   
 })
 function timeConverter(UNIX_timestamp) {
   var i = 0;
   var data = { list: [{ dt: UNIX_timestamp }] };
-  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var days = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
   var dayNum = new Date(data.list[i].dt * 1000).getDay();
   var result = days[dayNum];
   return result;
 }
+
