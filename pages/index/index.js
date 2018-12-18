b6f9f014cdce05380185333c4dfba831d6e2a769
hilary <hanhan0602@gmail.com>
2018-12-18 20:45:12 +0800

    加上hourly prediction list

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,14 +17,21 @@ Page({
           success: function (res) {
             var predictDatas = res.data.daily.data;
             for (var i = 0; i < predictDatas.length; i++) {
-              predictDatas[i].time = timeConverter(predictDatas[i].time)
+              predictDatas[i].time = timeConvertToWeekday(predictDatas[i].time)
               predictDatas[i].temperatureHigh = Math.round((predictDatas[i].temperatureHigh - 32) * 5 / 9)
               predictDatas[i].temperatureLow = Math.round((predictDatas[i].temperatureLow - 32) * 5 / 9)
               predictDatas[i].summary = (predictDatas[i].summary).split("。")[0]
             }
+            var hourlyDatas = res.data.hourly.data;
+            for (var i = 0; i < hourlyDatas.length; i++) {
+              hourlyDatas[i].time = timeConvertToHour(hourlyDatas[i].time)
+              hourlyDatas[i].apparentTemperature = Math.round((hourlyDatas[i].apparentTemperature - 32) * 5 / 9)
+            }
+            hourlyDatas[0].time = "現在";
             that.setData({
               temperature: Math.round((res.data.currently.temperature - 32) * 5 / 9),
               weather: res.data.currently.summary,
+              hourlyArray: hourlyDatas.slice(0,12),
               predictArray: predictDatas,
             })
           },
@@ -65,12 +72,23 @@ Page({
   
   
 })
-function timeConverter(UNIX_timestamp) {
+function timeConvertToWeekday(UNIX_timestamp) {
   var i = 0;
   var data = { list: [{ dt: UNIX_timestamp }] };
   var days = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
   var dayNum = new Date(data.list[i].dt * 1000).getDay();
   var result = days[dayNum];
   return result;
+}
+function timeConvertToHour(UNIX_timestamp) {
+  var date = new Date(UNIX_timestamp * 1000);
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? '下午' : '上午';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = ampm + hours + '時';
+  return strTime;
 }
 
